289c22f7735afa461a238d18614e36ca573038a7
Junming1120 <159005069+Junming1120@users.noreply.github.com>
2024-05-02 10:07:48 -0700

    1

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
 def A():
+    print("Hello")
 
 def B():
+    print("How are you")
 
 
 def C():
+    print("Hi")
+
+x = str(input("enter a letter "))
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
+
--- task2.py
+++ task2.py
@@ -8,3 +8,30 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+def title():
+    print("choose a random number between 1 and 100,guess the number in 10 turns")
+title()
+def game():
+    x = 54
+    c = 0 
+    y = int(input("enter a number"))
+    while x != y:
+        if x < y :
+            print("the number is over")
+            y = int(input("enter a number"))
+            c += 1 
+        else:
+            print("the number is smaller")
+            y = int(input("enter a number"))
+            c += 1
+        if c == 10:
+            print("You don't have chance!!!!!!!")
+            break
+    if x == y:
+        print("You got it!!!!!!!")
+game()
+            
+
+
+
